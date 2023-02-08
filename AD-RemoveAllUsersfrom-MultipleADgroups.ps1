0f55fba496962da44208051322a0aa8c84cc51d9
In8-EricFitch <eric.fitch@ineight.com>
2023-02-08 18:29:30 +0000

    Adding new scripts
    Adding new scripts

---
+++ AD-RemoveAllUsersfrom-MultipleADgroups.ps1
@@ -0,0 +1,33 @@
+﻿#################################
+### This removes ALL members from MULTIPLE Active Directory Groups.
+### Created By: Eric Fitch    8-Feb-2023
+################################
+
+#Variables - The AD group names.
+    $Group1 = "SG_SP_HR_Managers_with_Staff_in_AU"
+    $Group2 = "SG_SP_HR_Managers_with_Staff_in_CA"
+    $Group3  = "SG_SP_HR_Managers_with_Staff_in_EU"
+    $Group4  = "SG_SP_HR_Managers_with_Staff_in_SRI"
+    $Group5  = "SG_SP_HR_Managers_with_Staff_in_US"
+    $AllGroups = @($Group1,$Group2,$Group3,$Group4,$Group5)
+
+#Get a count of each group's members before taking action.
+foreach ($Group in $AllGroups) {
+    $GroupMembers = Get-ADGroupMember -Identity $Group
+    Write-Host "BEFORE taking action on $Group, the group's membership =" $GroupMembers.Count
+    }
+
+#The magic happens here.  Remove ALL members from ALL of the groups.
+foreach ($Group in $AllGroups) {
+    Remove-ADGroupMember -Identity $Group -Members (Get-ADGroupMember -Identity $Group) -Confirm
+    }
+
+#Get a count of each group's members after taking action.
+foreach ($Group in $AllGroups) {
+    $GroupMembers = Get-ADGroupMember -Identity $Group
+    Write-Host "AFTER taking action on $Group, the group's membership =" $GroupMembers.Count
+    }
+
+##########################################
+### The End.  Cert Signature below.
+##########################################
---
+++ AD-RemoveAllUsersfrom-OneADgroup.ps1
@@ -0,0 +1,39 @@
+﻿#################################
+### This removes ALL members from ONE Active Directory Group.  The user is prompted to enter the AD group name.
+### Created By: Eric Fitch   8-Feb-2023
+################################
+
+#Start logging
+    $ScriptName = "AD-RemoveAllUsersfrom-OneADgroup"    #Update this to match the script name!!
+    $Timestamp = Get-Date -Format "yyyy-MMM-dd--HH.MM.s"
+    $FileName = $ScriptName + "-" + $Timestamp + ".txt"
+    $LoggingPath = "C:\Temp\ScriptLogs\" + $FileName
+    Start-Transcript -Path $LoggingPath
+
+#Variables - Get Input from User.
+    $ADGroup = Read-Host  "Please enter the name of the group, like this:  SG_groupName   --->>"
+
+#Get a count of the group members before taking action.
+    $ADGroupMembers = Get-ADGroupMember -Identity $ADGroup
+    Write-Host "**BEFORE taking action, the group's membership count =" $ADGroupMembers.Count
+
+#For the Log, record the AD group members before taking any action.
+    Write-Host "**The group originally contained these members:"
+    foreach ($Member in $ADGroupMembers) {
+        $Name = Get-ADUser -Identity $Member -Properties SamAccountName | Select-Object SamAccountName
+        Write-Host $Name.SamAccountName
+        }
+
+#The magic happens here.  Remove ALL members from the group.
+    Remove-ADGroupMember -Identity $ADGroup -Members (Get-ADGroupMember -Identity $ADGroup) -Confirm
+
+#Get a count of the group members after taking action.
+    $ADGroupMembers = Get-ADGroupMember -Identity $ADGroup
+    Write-Host "**AFTER taking action, the group's membership count =" $ADGroupMembers.Count
+
+#Stop logging
+    Stop-Transcript
+
+##########################################
+### The End.  Cert Signature below.
+##########################################
